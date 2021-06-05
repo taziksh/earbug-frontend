7bc59312134412883b6690361bac234ef066aa59
Tazik Shahjahan <35576188+ZKTKZ@users.noreply.github.com>
2021-06-05 18:12:34 -0400

    Codesandbox fork test1

--- src/SuggestExample.tsx
+++ src/SuggestExample.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import { debounce } from 'lodash';
-import { Suggest } from '@blueprintjs/select';
+import * as React from "react";
+import { debounce } from "lodash";
+import { Suggest } from "@blueprintjs/select";
 
-import './app.scss';
+import "./app.scss";
 
-import { CardExample } from './CardExample';
-import { Player } from './Player';
-import * as Songs from './songs';
+import { CardExample } from "./CardExample";
+import { Player } from "./Player";
+import * as Songs from "./songs";
 
-require('dotenv').config();
-let Spotify = require('spotify-web-api-js');
+require("dotenv").config();
+let Spotify = require("spotify-web-api-js");
 let spotify = new Spotify();
 
 const songSuggestStyles = {
-  display: 'flex',
-  justifyContent: 'center',
-  marginBottom: '5%'
+  display: "flex",
+  justifyContent: "center",
+  marginBottom: "5%"
 };
 
 const SongSuggest = Suggest.ofType<Songs.InterfaceSong>();
@@ -48,13 +48,13 @@ export class SuggestExample extends React.PureComponent<
               itemRenderer={Songs.renderSong}
               onItemSelect={this.handleItemSelect}
               inputValueRenderer={this.handleValueRender}
-              onQueryChange={this.debounceHandleQueryChange}
-              // resetOnClose={true}
+              onQueryChange={this.handleQueryChange}
+              resetOnClose={true}
               fill={false}
               popoverProps={{ minimal: true }}
             />
           </div>
-          <Player song={this.state.song}/>
+          <Player song={this.state.song} />
         </CardExample>
       </div>
     );
@@ -64,33 +64,34 @@ export class SuggestExample extends React.PureComponent<
     this.setState((prevState) => {
       return { ...prevState, song: song };
     });
-  }
+  };
   private handleValueRender = (item: Songs.InterfaceSong) => {
     return JSON.stringify(item.title);
-  }
+  };
 
   // async Spotify API caller
   private handleQueryChange = async (song: Songs.InterfaceSong) => {
+    console.log("Query: ", song);
     let accessToken = null;
     let results: Songs.InterfaceSong[] = [];
 
-    await fetch('https://accounts.spotify.com/api/token', {
-      body: 'grant_type=client_credentials',
+    await fetch("https://accounts.spotify.com/api/token", {
+      body: "grant_type=client_credentials",
       headers: {
         Authorization:
-          'Basic ZmE5Y2IwN2M4ZmZjNDM1ZjlhMWIzZTk4NGZhYWYyOWM6OTM3ODk4Njc1NDU3NGI4ZTk3ZGVkMThhNTc2ZGI3ZTQ=',
-        'Content-Type': 'application/x-www-form-urlencoded'
+          "Basic ZmE5Y2IwN2M4ZmZjNDM1ZjlhMWIzZTk4NGZhYWYyOWM6OTM3ODk4Njc1NDU3NGI4ZTk3ZGVkMThhNTc2ZGI3ZTQ=",
+        "Content-Type": "application/x-www-form-urlencoded"
       },
-      method: 'POST'
+      method: "POST"
     })
       .then((response) => response.json())
       .then((data) => {
-        let accessTokenField = 'access_token';
+        let accessTokenField = "access_token";
         accessToken = data[accessTokenField];
         spotify.setAccessToken(accessToken);
         spotify.searchTracks(song).then(
           (data2) => {
-            // console.log('Searching for ', song, data);
+            console.log("Searching for ", song, data);
             let tracks = data2.tracks.items;
             tracks.forEach((track) => {
               let result: Songs.InterfaceSong = {
@@ -105,18 +106,18 @@ export class SuggestExample extends React.PureComponent<
             });
           },
           (err) => {
-            handleError('Spotify Error: ', err);
+            handleError("Spotify Error: ", err);
           }
         );
       })
       .catch((err) => {
-        handleError('Error: ', err);
+        handleError("Error: ", err);
       });
-  }
+  };
 
   // debounce Spotify API requests
-  private debounceHandleQueryChange = () => debounce(
-    this.handleQueryChange,
-    500
-  )
+  private debounceHandleQueryChange = () => {
+    console.log("Debouncing now...");
+    debounce(this.handleQueryChange, 500);
+  };
 }
